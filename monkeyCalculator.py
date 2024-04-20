c141774e6969e3d1f5c2d0e0ce3cfec555cf3113
tlneff <neff.tyler@gmail.com>
2024-04-19 19:30:51 -0500

    added program logic

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -1,14 +1,16 @@
 def calculateTime():
-    
+
     # This first line is provided for you
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
-
+    if (monkey_one == 'y' and monkey_two == 'y') or (monkey_one == 'n' and monkey_two == 'n'):
+        print("Uh Opyh! We're in trouble!")
+    else:
+        print("Yay! We're going to have a good day!")
     # end assignment
 
 
-## If you want to test locally run > python monkeyCalculator.py
+# If you want to test locally run > python monkeyCalculator.py
 
 if __name__ == "__main__":
     calculateTime()
-    
